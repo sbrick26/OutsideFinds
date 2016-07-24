2f515d04f3a786ee1900acdf095348cb48d36e07
sbrick26 <swayambarik@gmail.com>
2016-07-23 18:36:21 -0700

    Added post class in parse and create view,

---
+++ OutsideFinds/CreateViewController.swift
@@ -0,0 +1,70 @@
+//
+//  CreateViewController.swift
+//  OutsideFinds
+//
+//  Created by Swayam Barik on 7/23/16.
+//  Copyright © 2016 Swayam Barik. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class CreateViewController: UIViewController {
+
+    @IBOutlet weak var itemNameField: UITextField!
+    @IBOutlet weak var bountyField: UITextField!
+    @IBOutlet weak var descriptionView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func createPressed(sender: UIButton) {
+        if itemNameField.text != "" && bountyField.text != "" && descriptionView.text != "" {
+            
+            let newPost = PFObject(className: "Post")
+            newPost["itemName"] = itemNameField.text
+            newPost["itemDescription"] = descriptionView.text
+            newPost["itemPrice"] = bountyField.text
+            newPost["user"] = PFUser.currentUser()
+            newPost["claimed"] = false
+            
+            
+            newPost.saveInBackgroundWithBlock { (success: Bool, erro: NSError?) -> Void in
+                if success {
+                    print("Object Saved!!")
+                    let vc : AnyObject! = self.storyboard!.instantiateViewControllerWithIdentifier("Feed View Controller") // again change to your view
+                    self.showViewController(vc as! FeedViewController, sender: vc) // change again
+                    
+                }
+                else {
+                    print("Error!!")
+                }
+            
+            
+            
+            
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
